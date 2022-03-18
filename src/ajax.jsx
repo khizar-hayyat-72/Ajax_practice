a80f419fa880bd5aa18aaa8fc2c1e7bb49de69bf
khizar-hayyat-72 <Khizarhayyt199093@gmail.com>
2022-03-18 15:47:08 +0500

    Updated

--- src/ajax.jsx
+++ src/ajax.jsx
@@ -1,38 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import Container from '@mui/material/Container'
+import axios from 'axios'
 
 
 const Ajax = () => {
 
-  const[users, setUsers] = useState([]);
-
-const getUsers = async() => {
-  const response =await fetch('https://dev-api.amazepbc.com/mazapi/v1/values/states');
-  setUsers(await response.json());
-}
-
   useEffect(() => {
-    getUsers();
-  }, []);
+    async function getData(){
+      const res = await axios.get(`https://dev-api.amazepbc.com/mazapi/v1/values/states`, { mode: 'no-cors' });
+      console.log(res.data.id)
+    }
 
+    getData();
+  })
   return (
     <Container>
       <>
          <h2>Amaze Users</h2>
-            {
-            
-               users.map((curElem, index) => {
-                  return(
-                    <>
-                      <ul>
-                        <li>{curElem.id}</li>
-                      </ul>
-                    </>
-                  )
-               })
-            
-            
-            }
       </>
     </Container>
   )
